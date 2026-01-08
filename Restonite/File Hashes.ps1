42d316a1747f0450a51f1dbc303732648d68dc01
Uruloke <38957400+Uruloke@users.noreply.github.com>
2026-01-08 18:31:19 +0100

    Remove unused file

--- Restonite/File Hashes.ps1
+++
@@ -1,2 +0,0 @@
-"**MD5:** {0}" -f $($(CertUtil -hashfile bin\Release\net472\Restonite.dll MD5)[1] -replace " ","")
-"**SHA256:** {0}" -f $($(CertUtil -hashfile bin\Release\net472\Restonite.dll SHA256)[1] -replace " ","")
