cb96149c3ff8a782470613657daca6f115e95d6b
Uruloke <38957400+Uruloke@users.noreply.github.com>
2026-07-08 19:33:45 +0200

    Fixed last configuration not being parsed properly

--- Restonite/Avatar_LastConfiguration.cs
+++ Restonite/Avatar_LastConfiguration.cs
@@ -35,11 +35,13 @@ internal partial class Avatar
             for(var set = 0; set < map.MaterialSets.Count; set++)
             {
                 var setSlot = meshRendererSlot.AddSlot($"<color=hero.green>Material Set {set}</color>");
-                meshRendererSlot.Tag_Field.Value = null!;
+                setSlot.Tag_Field.Value = null!;
+                setSlot.OrderOffset_Field.Value = set;
 
                 for(var index = 0; index < map.MaterialSets[set].Count; index++)
                 {
                     var materialSlot = setSlot.AddSlot($"<color=hero.cyan>Material {index}</color>");
+                    materialSlot.OrderOffset_Field.Value = index;
 
                     var materialDynVarSpace = materialSlot.AttachComponent<DynamicVariableSpace>();
                     materialDynVarSpace.SpaceName.Value = "MaterialSlotConfig";
@@ -72,46 +74,94 @@ internal partial class Avatar
 
         foreach(var meshRendererSlot in slot.GetChildrenWithTag("StatueSystemMeshRendererConfiguration"))
         {
+            Log.Debug($"Trying to parse configuration in {meshRendererSlot.ToShortString()}");
+
             var dynVarSpace = DynamicVariableHelper.FindSpace(meshRendererSlot, "MeshRendererConfig");
             if(dynVarSpace is null)
+            {
+                Log.Warn($"Couldn't find MeshRendererConfig dynvar space at {meshRendererSlot.ToShortString()}");
                 continue;
+            }
 
             var meshRendererMap = new MeshRendererMap();
 
             if(dynVarSpace.TryReadValue<MaterialSet>("NormalMaterialSet", out var normalMaterialSet))
                 meshRendererMap.NormalMaterialSet = normalMaterialSet;
+            else
+                Log.Warn($"Couldn't read NormalMaterialSet variable at {meshRendererSlot.ToShortString()}");
+
             if(dynVarSpace.TryReadValue<MeshRenderer>("NormalMeshRenderer", out var normalMeshRenderer))
                 meshRendererMap.NormalMeshRenderer = normalMeshRenderer;
+            else
+                Log.Warn($"Couldn't read NormalMeshRenderer variable at {meshRendererSlot.ToShortString()}");
+
             if(dynVarSpace.TryReadValue<Slot>("NormalSlot", out var normalSlot))
                 meshRendererMap.NormalSlot = normalSlot;
+            else
+                Log.Warn($"Couldn't read NormalSlot variable at {meshRendererSlot.ToShortString()}");
+
             if(dynVarSpace.TryReadValue<MaterialSet>("StatueMaterialSet", out var statueMaterialSet))
                 meshRendererMap.StatueMaterialSet = statueMaterialSet;
+            else
+                Log.Warn($"Couldn't read StatueMaterialSet variable at {meshRendererSlot.ToShortString()}");
+
             if(dynVarSpace.TryReadValue<MeshRenderer>("StatueMeshRenderer", out var statueMeshRenderer))
                 meshRendererMap.StatueMeshRenderer = statueMeshRenderer;
+            else
+                Log.Warn($"Couldn't read StatueMeshRenderer variable at {meshRendererSlot.ToShortString()}");
+
             if(dynVarSpace.TryReadValue<Slot>("StatueSlot", out var statueSlot))
                 meshRendererMap.StatueSlot = statueSlot;
+            else
+                Log.Warn($"Couldn't read StatueSlot variable at {meshRendererSlot.ToShortString()}");
+
 
             for(var set = 0; set < meshRendererSlot.ChildrenCount; set++)
             {
                 var setSlot = meshRendererSlot[set];
                 var materials = new List<MaterialMap>();
 
+                Log.Debug($"Found material set configuration at {setSlot.ToShortString()}");
+
                 for(var index = 0; index < setSlot.ChildrenCount; index++)
                 {
-                    var indexSlot = meshRendererSlot[set][index];
+                    var indexSlot = setSlot[index];
                     var material = new MaterialMap();
                     var materialDynVarSpace = DynamicVariableHelper.FindSpace(indexSlot, "MaterialSlotConfig");
 
+                    Log.Debug($"Trying to parse material configuration in {indexSlot.ToShortString()}");
+
+                    if(materialDynVarSpace is null)
+                    {
+                        Log.Warn($"Couldn't find MaterialSlotConfig dynvar space at {indexSlot.ToShortString()}");
+                        continue;
+                    }
+
                     if(materialDynVarSpace.TryReadValue<bool>("Clothes", out var clothes))
                         material.Clothes = clothes;
+                    else
+                        Log.Warn($"Couldn't read Clothes variable at {indexSlot.ToShortString()}");
+
                     if(materialDynVarSpace.TryReadValue<IAssetProvider<Material>>("Normal", out var normal))
                         material.Normal = normal;
+                    else
+                        Log.Warn($"Couldn't read Normal variable at {indexSlot.ToShortString()}");
+
                     if(materialDynVarSpace.TryReadValue<IAssetProvider<Material>>("Statue", out var statue))
                         material.Statue = statue;
+                    else
+                        Log.Warn($"Couldn't read Statue variable at {indexSlot.ToShortString()}");
+
                     if(materialDynVarSpace.TryReadValue<int>("TransitionType", out var transitionType))
                         material.TransitionType = (StatueType)transitionType;
+                    else
+                        Log.Warn($"Couldn't read TransitionType variable at {indexSlot.ToShortString()}");
+
                     if(materialDynVarSpace.TryReadValue<bool>("UseAsIs", out var useAsIs))
                         material.UseAsIs = useAsIs;
+                    else
+                        Log.Warn($"Couldn't read UseAsIs variable at {indexSlot.ToShortString()}");
+
 
                     materials.Add(material);
                 }
@@ -119,8 +169,12 @@ internal partial class Avatar
                 meshRendererMap.MaterialSets.Add(materials);
             }
 
+            Log.Info($"Found mesh renderer with {meshRendererMap.MaterialSets.Count} material sets and {meshRendererMap.MaterialSets.SelectMany(x => x).Count()} materials");
+
             _lastConfiguration.Add(meshRendererMap);
         }
+
+        Log.Info($"Successfully parsed configuration for {_lastConfiguration.Count} mesh renderers");
     }
 
     private MeshRendererMap? FindConfiguration(MeshRenderer? meshRenderer)
